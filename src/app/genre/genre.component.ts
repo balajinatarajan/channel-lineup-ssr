0be7781f0f07e6224a75d9a23b93e81646db652d
balajinatarajan <bala@balajis-mbp.corp.siriusxm.com>
2018-07-10 17:06:43 -0400

    WORKING - same component reload

--- src/app/genre/genre.component.ts
+++ src/app/genre/genre.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router'; 
 import { Channel } from '../channel';
 import { ChannelService } from '../channel.service';
 
@@ -13,13 +13,38 @@ export class GenreComponent implements OnInit {
   category = "";
   genre = "";
   channels: Channel[];
-  constructor(private route: ActivatedRoute, private channelService: ChannelService) { }
+  navigationSubscription;
+
+  constructor(private route: ActivatedRoute, private channelService: ChannelService, private router: Router) {
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {
+      // If it is a NavigationEnd event re-initalise the component
+      if (e instanceof NavigationEnd) {
+        this.initialiseInvites();
+      }
+    });
+  }
 
   ngOnInit() {
+    this.getChannels();
+  }
+  initialiseInvites() {
+    // Set default values and re-fetch any data you need.
+    this.getChannels();
+  }
+  getChannels() {
     this.category = this.route.snapshot.paramMap.get('category');
     this.genre = this.route.snapshot.paramMap.get('genre');
     this.channelService.getChannelsByGenre(this.genre)
       .subscribe(channels => this.channels = channels);
+  }
+
+  ngOnDestroy() {
+    // avoid memory leaks here by cleaning up after ourselves. If we  
+    // don't then we will continue to run our initialiseInvites()   
+    // method on every navigationEnd event.
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
   }
 
 }
